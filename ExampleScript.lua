7008d77c896a1724c7d484b194e3123f7617c166
Fallen Angel <98043100+fallenghost12@users.noreply.github.com>
2023-01-21 23:24:50 -0800

    Update ExampleScript.lua

--- ExampleScript.lua
+++ ExampleScript.lua
@@ -1,17 +1,24 @@
 --// These scripts won't work as they will be needing alot of instances
 
+
+--// Services
 local MarketplaceService = game:GetService("MarketplaceService")
 local player = game.Players.LocalPlayer
+
+--// UI
 local Main = script.Parent
 
+--// UI Buttons
 local Bottom = script.Parent.BottomFrame
 local Settings = Bottom.Settings
 local Store = Bottom.Store
 
+--// Other UI Frames
 local SettingsFrame = Main.Settings
 local Donations = Main.DonationHub
 local Tween = game:GetService('TweenService')
 
+--// Workspace GridStructure
 local GridPart = workspace.GridTexture.Part
 
 
@@ -20,24 +27,32 @@ local DonationProducts = {
 	["25"] = 1;
 }
 
+--// Prompt to product
 local function promptPurchase(productId)
 	MarketplaceService:PromptProductPurchase(player, productId)
 end
 
+--// Local Bool Values
 local SettingsToggle = true
 local StoreToggle = true
+
+--// On Mouse1 Connections
 Settings.MouseButton1Down:Connect(function()
 	--Main.Settings.Visible = not Main.Settings.Visible
+		--// Making Bool = Not Bool
 	SettingsToggle = not SettingsToggle
 	if not SettingsToggle then
+			--// Basically Animations
 		if not StoreToggle then
 			StoreToggle = true
+				--// Tween
 			Tween:Create(Donations, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(2.1, 0, 0.5, 0)}):Play()
 		end
 		SettingsFrame.Position = UDim2.new(-1.5, 0, 0.5, 0)
 		Tween:Create(SettingsFrame, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(0.5, 0, 0.5, 0)}):Play()
 		wait(0.2)
 	else
+			--// Tween
 		Tween:Create(SettingsFrame, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(2.1, 0, 0.5, 0)}):Play()
 	end
 end)
@@ -46,18 +61,23 @@ Store.MouseButton1Down:Connect(function()
 	--Main.DonationHub.Visible = not Main.DonationHub.Visible
 	StoreToggle = not StoreToggle
 	if not StoreToggle then
+			--// Basically Animations!
 		if not SettingsToggle then
 			SettingsToggle = true
+				--// Tween
 			Tween:Create(SettingsFrame, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(2.1, 0, 0.5, 0)}):Play()
 		end
 		Donations.Position = UDim2.new(-1.5, 0, 0.5, 0)
+			--// Tween
 		Tween:Create(Donations, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(0.5, 0, 0.5, 0)}):Play()
 		wait(0.2)
 	else
+			--// Tween
 		Tween:Create(Donations, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(2.1, 0, 0.5, 0)}):Play()
 	end
 end)
 
+--// Tween & Button1
 SettingsFrame.Background1.TextButton.MouseButton1Down:Connect(function()
 	Tween:Create(SettingsFrame, TweenInfo.new(0.35, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Position = UDim2.new(2.1, 0, 0.5, 0)}):Play()
 	SettingsToggle = true
@@ -69,12 +89,14 @@ Donations.Background1.TextButton.MouseButton1Down:Connect(function()
 end)
 
 local Gridtoggle = true
+--// Tween & Button1
 SettingsFrame.Background2.ScrollingFrame.GridTextureSet.ToggleClick.MouseButton1Down:Connect(function()
 	Gridtoggle = not Gridtoggle
 	if not Gridtoggle then
 		--GridPart.Parent = nil
 		for i,v in ipairs(GridPart:GetChildren()) do
 			task.spawn(function()
+						--// Make the transparency 1 with animations and 0.25 timeline
 				Tween:Create(v, TweenInfo.new(0.25), {Transparency = 1}):Play()
 			end)
 		end
@@ -84,6 +106,7 @@ SettingsFrame.Background2.ScrollingFrame.GridTextureSet.ToggleClick.MouseButton1
 		--GridPart.Parent = workspace.GridTexture
 		for i,v in ipairs(GridPart:GetChildren()) do
 			task.spawn(function()
+						-- redo to normal
 				Tween:Create(v, TweenInfo.new(0.25), {Transparency = 0.65}):Play()
 			end)
 		end
@@ -93,9 +116,11 @@ SettingsFrame.Background2.ScrollingFrame.GridTextureSet.ToggleClick.MouseButton1
 end)
 
 local TrailToggle = true
+--// Tween & Button1
 SettingsFrame.Background2.ScrollingFrame.PlanetTrailSets.ToggleClick.MouseButton1Down:Connect(function()
 	TrailToggle = not TrailToggle
 	if not TrailToggle then
+			--// Making all the workspace.System folder trails to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Trail') then
 				v.Enabled = false
@@ -104,6 +129,7 @@ SettingsFrame.Background2.ScrollingFrame.PlanetTrailSets.ToggleClick.MouseButton
 		SettingsFrame.Background2.ScrollingFrame.PlanetTrailSets.ToggleClick.Toggle.BackgroundColor = BrickColor.new(21)
 		SettingsFrame.Background2.ScrollingFrame.PlanetTrailSets.ToggleClick.Toggle.Button.Position = UDim2.new(0, 0, 0, 0)
 	else
+			--// Making all the workspace.System folder trails to be true
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Trail') then
 				v.Enabled = true
@@ -118,6 +144,7 @@ local Highlights = true
 SettingsFrame.Background2.ScrollingFrame.PlanetHighlightTrails.ToggleClick.MouseButton1Down:Connect(function()
 	Highlights = not Highlights
 	if not Highlights then
+			--// Making all the workspace.System folder highlights to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v.Name == "Highlight" then
 				v.Enabled = false
@@ -126,6 +153,7 @@ SettingsFrame.Background2.ScrollingFrame.PlanetHighlightTrails.ToggleClick.Mouse
 		SettingsFrame.Background2.ScrollingFrame.PlanetHighlightTrails.ToggleClick.Toggle.BackgroundColor = BrickColor.new(21)
 		SettingsFrame.Background2.ScrollingFrame.PlanetHighlightTrails.ToggleClick.Toggle.Button.Position = UDim2.new(0, 0, 0, 0)
 	else
+			--// Making all the workspace.System folder highlights to be true
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v.Name == "Highlight" then
 				v.Enabled = true
@@ -140,6 +168,7 @@ local NameToggle = true
 SettingsFrame.Background2.ScrollingFrame.PlanetNamesSet.ToggleClick.MouseButton1Down:Connect(function()
 	NameToggle = not NameToggle
 	if not NameToggle then
+			--// Making all the workspace.System folder bilboard/names to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v.Name == "BillboardGui" then
 				v.Enabled = false
@@ -148,6 +177,7 @@ SettingsFrame.Background2.ScrollingFrame.PlanetNamesSet.ToggleClick.MouseButton1
 		SettingsFrame.Background2.ScrollingFrame.PlanetNamesSet.ToggleClick.Toggle.BackgroundColor = BrickColor.new(21)
 		SettingsFrame.Background2.ScrollingFrame.PlanetNamesSet.ToggleClick.Toggle.Button.Position = UDim2.new(0, 0, 0, 0)
 	else
+			--// Making all the workspace.System folder bilboard/names to be true
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v.Name == "BillboardGui" then
 				v.Enabled = true
@@ -162,6 +192,7 @@ local BeamToggle = true
 SettingsFrame.Background2.ScrollingFrame.PlanetPositionBeam.ToggleClick.MouseButton1Down:Connect(function()
 	BeamToggle = not BeamToggle
 	if not BeamToggle then
+			--// Making all the workspace.System folder beams/planetbeams name to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Beam') and v.Parent.Name == "PlanetBeamLocation" then
 				v.Enabled = false
@@ -170,6 +201,7 @@ SettingsFrame.Background2.ScrollingFrame.PlanetPositionBeam.ToggleClick.MouseBut
 		SettingsFrame.Background2.ScrollingFrame.PlanetPositionBeam.ToggleClick.Toggle.BackgroundColor = BrickColor.new(21)
 		SettingsFrame.Background2.ScrollingFrame.PlanetPositionBeam.ToggleClick.Toggle.Button.Position = UDim2.new(0, 0, 0, 0)
 	else
+			--// Making all the workspace.System folder breams to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Beam') and v.Parent.Name == "PlanetBeamLocation" then
 				v.Enabled = true
@@ -184,12 +216,14 @@ local AxisToggle = true
 SettingsFrame.Background2.ScrollingFrame.PlanetAxisSet.ToggleClick.MouseButton1Down:Connect(function()
 	AxisToggle = not AxisToggle
 	if not AxisToggle then
+			--// Making all the workspace.System folder axis name (beams) to be false
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Beam') and v.Parent.Name == "Axis" then
 				v.Enabled = false
 			end
 		end
 	else
+			--// Making all the workspace.System folder axis name (beams) to be true
 		for i,v in ipairs(workspace.System:GetDescendants()) do
 			if v:IsA('Beam') and v.Parent.Name == "Axis" then
 				v.Enabled = true
@@ -200,6 +234,7 @@ end)
 
 local ArrowVis = false
 
+-- Tween animations for the UI
 Main.ArrowFrame.UpArrow.MouseButton1Down:Connect(function()
 	ArrowVis = true
 	if ArrowVis then
@@ -212,6 +247,7 @@ Main.ArrowFrame.UpArrow.MouseButton1Down:Connect(function()
 	end
 end)
 
+-- Tween animations for the UI
 Main.ArrowFrame.DownArrow.MouseButton1Down:Connect(function()
 	ArrowVis = false
 	if not ArrowVis then
@@ -224,6 +260,9 @@ Main.ArrowFrame.DownArrow.MouseButton1Down:Connect(function()
 	end
 end)
 
+-- Basically updating the Leaderboard ui from the workspace.Donators folder
+-- We are getting the children from the folder, then adding them to a table, then we are sorting the table by table.sort
+-- after sorting we are reading the table and then making instances (template)
 local function UpdateLeaderBoard()
 	
 	local d = {}
@@ -270,16 +309,20 @@ local function UpdateLeaderBoard()
 	
 end
 
+-- If a child is added to the folder, update again
 workspace.Donators.ChildAdded:Connect(function()
 	UpdateLeaderBoard()
 end)
 
+-- If a child is removed to the folder, update again
 workspace.Donators.ChildRemoved:Connect(function()
 	UpdateLeaderBoard()
 end)
 
+-- Update for the first time
 UpdateLeaderBoard()
 
+-- If a donations button is pressed, prompt purchase.
 for i,v in pairs(Donations.Background2.ButtonList:GetChildren()) do
 	if v:IsA('TextButton') then
 		v.MouseButton1Down:Connect(function()
@@ -288,6 +331,8 @@ for i,v in pairs(Donations.Background2.ButtonList:GetChildren()) do
 	end
 end
 
+
+-- We added clickdetectors tot he system models, if the click detector is touched, we show specific info UI about the planet. (tween animations)
 for j,k in pairs(workspace.System:GetChildren()) do
 	if k:IsA('Model') then
 		for i2,v2 in pairs(k:GetChildren()) do
@@ -311,6 +356,7 @@ for j,k in pairs(workspace.System:GetChildren()) do
 	end
 end
 
+-- On pressing the (X) exit button in ui, exists
 for l,o in pairs(script.Parent.Planets:GetChildren()) do
 	for m, n in pairs(o:GetChildren()) do
 		if n.Name == "Background1" then
